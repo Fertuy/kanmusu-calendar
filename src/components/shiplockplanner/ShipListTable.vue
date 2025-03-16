fb0e3b20dce686ef6a4ade76d2811fad54437282
Fertuy <paliy_ivan@ukr.net>
2025-03-16 18:17:25 +0900

    Implemented: - VInfiniteScroll Reworked - improved performance speed for drag and drop

--- src/components/shiplockplanner/ShipListTable.vue
+++ src/components/shiplockplanner/ShipListTable.vue
@@ -2,31 +2,68 @@
   setup
   lang="ts"
 >
-  import { useShiplist } from '@/stores/shiplist'
+  import { Kanmusu, useShiplist } from '@/stores/shiplist'
   import ShipListTableFilter from '@/components/shiplockplanner/ShipListTableFilter.vue'
+  import { Ref } from 'vue'
 
   const shiplist = useShiplist()
   const displayFilteredShipList = computed(() => shiplist.filteredShiplist)
+
+  const loadIndex = ref(20)
+  const displayedShipList: Ref<Array<Kanmusu>> = ref([])
+
+  const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
+  onMounted(() => {
+    displayedShipList.value = displayFilteredShipList.value.slice(0, 20)
+  })
+
+  watch(displayFilteredShipList, async () => {
+    displayedShipList.value = []
+    await delay(10)
+    loadIndex.value = 20
+    displayedShipList.value = displayFilteredShipList.value.slice(0, 20)
+  })
+
+  function loadMoreShips ({ done }:any) {
+    console.log(`displayedShipList: ${displayedShipList.value.length}`)
+    console.log(`displayFilteredShipList: ${displayFilteredShipList.value.length}`)
+    if (displayedShipList.value.length < displayFilteredShipList.value.length) {
+      const nextShips = displayFilteredShipList.value.slice(loadIndex.value, loadIndex.value + 10)
+      displayedShipList.value.push(...nextShips)
+      loadIndex.value += 10
+      done('ok')
+      return
+    }
+    done('empty')
+  }
 
 </script>
 <template>
   <VContainer class="w-auto d-flex flex-wrap">
     <ShipListTableFilter />
-    <div
-      v-if="displayFilteredShipList.length > 0"
-      class="w-auto d-flex flex-wrap content"
+    <VInfiniteScroll
+      v-if="displayedShipList.length > 0"
+      class="w-100"
+      direction="vertical"
+      :items="displayedShipList"
+      @load="loadMoreShips"
     >
-      <ShipCard
-        v-for="ship in displayFilteredShipList"
-        :key="ship.uniqueId"
-        :ship="ship"
-      />
-    </div>
+      <div class="w-auto d-flex flex-wrap content">
+        <template
+          v-for="ship in displayedShipList"
+          :key="ship.uniqueId"
+        >
+          <ShipCard
+            :ship="ship"
+          />
+        </template>
+      </div>
+    </VInfiniteScroll>
   </VContainer>
 </template>
 <style scoped lang="scss">
 .content {
-  margin: -4px;
   flex-grow: 1;
 }
 </style>
